dbf49f5331824c11aeb419c5b19ee3aad5a170d7
Noskcaj <kcajdev@gmail.com>
2023-04-22 12:17:21 -0400

    Add additional belt speeds, add flip control

--- control.lua
+++ control.lua
@@ -16,6 +16,15 @@ local scaffold_map_gen_settings = {
   starting_area = "none",
 }
 
+local function create_level_display(player)
+  local root = player.gui.top.level
+  if root then
+    player.gui.top.level.destroy()
+  end
+
+  player.gui.top.add { type = "label", name = "level", caption = "Level 0" }
+end
+
 script.on_init(function(data)
   if not global.surfaces or global.surfaces[0] then
     local nauvis_idx = game.get_surface('nauvis').index
@@ -24,7 +33,7 @@ script.on_init(function(data)
   end
 
   for _, player in pairs(game.players) do
-      create_level_display(player)
+    create_level_display(player)
   end
 end)
 
@@ -32,36 +41,28 @@ script.on_event(defines.events.on_player_created, function(event)
   create_level_display(game.players[event.player_index])
 end)
 
-function create_level_display(player)
-  local root = player.gui.top.level
-  if root then
-    player.gui.top.level.destroy()
-  end
 
-  local root = player.gui.top.add{type="label",name="level",caption="Level 0"}
-end
-
-function update_gui(player, new_level)
+local function update_gui(player, new_level)
   if not player.gui.top.level then
     create_level_display(player)
   end
   player.gui.top.level.caption = "Level " .. new_level
 end
 
-function table_invert(t)
-   local s = {}
-   for k, v in pairs(t) do
-     s[v] = k
-   end
-   return s
+local function table_invert(t)
+  local s = {}
+  for k, v in pairs(t) do
+    s[v] = k
+  end
+  return s
 end
 
-function level_from_surface(idx)
+local function level_from_surface(idx)
   local inv_surfaces = table_invert(global.surfaces)
   return inv_surfaces[idx]
 end
 
-function flip_direction(dir)
+local function flip_direction(dir)
   if dir == defines.direction.north then
     return defines.direction.south
   elseif dir == defines.direction.south then
@@ -75,24 +76,24 @@ end
 
 ---@param event EventData.on_built_entity | EventData.on_robot_built_entity
 ---@param err string
-function prevent_construction(event, err)
+local function prevent_construction(event, err)
   local entity = event.created_entity
   local player = nil
   if event.player_index then
     player = game.players[event.player_index]
   end
-  local stack = {name=entity.prototype.mineable_properties.products[1].name, amount=1}
+  local stack = { name = entity.prototype.mineable_properties.products[1].name, amount = 1 }
   if event.name == defines.events.on_built_entity
-    and player
-    and player.can_insert(stack) then
-    player.create_local_flying_text{text=err,create_at_cursor=true}
+      and player
+      and player.can_insert(stack) then
+    player.create_local_flying_text { text = err, create_at_cursor = true }
     player.insert(stack)
   else
     -- TODO: doesn't work?
-    local ground_item = entity.surface.create_entity{
+    local ground_item = entity.surface.create_entity {
       name = "item-on-ground",
       position = entity.position,
-      stack = stack}
+      stack = stack }
     if ground_item and ground_item.valid then
       ground_item.order_deconstruction(entity.force)
     end
@@ -100,49 +101,59 @@ function prevent_construction(event, err)
   entity.destroy()
 end
 
+---@param up string
+---@return string
+local function down_belt_name_from_up(up)
+  local name = string.gsub(up, "(%w*%-?)up%-belt", "%1down-belt")
+  return name
+end
+
+---@param down string
+---@return string
+local function up_belt_name_from_down(down)
+  local name = string.gsub(down, "(%w*%-?)down%-belt", "%1up-belt")
+  return name
+end
+
 local error_no_z_level = "Unable to create multiple z-levels here"
 local error_collision = "Collision with entity in other z-level"
 
-script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity}, function(event)
+script.on_event({ defines.events.on_built_entity, defines.events.on_robot_built_entity }, function(event)
   local entity = event.created_entity
-  local player = nil
-  if event.player_index then
-    player = game.players[event.player_index]
-  end
-  if entity.name == "down-belt" then
+  if entity.name:match('%w*down%-belt$') then
     entity.linked_belt_type = "input"
     local current_level = level_from_surface(entity.surface_index)
 
     if not current_level then
-      prevent_construction(event,error_no_z_level)
+      prevent_construction(event, error_no_z_level)
       return
     end
-    
+
     local surface = global.surfaces[current_level - 1] and game.get_surface(global.surfaces[current_level - 1])
     if not surface then
       surface = game.create_surface("Level " .. current_level - 1, empty_map_gen_settings)
       global.surfaces[current_level - 1] = surface.index
     end
-
-    if not surface.can_place_entity{name="up-belt", position=entity.position} then
-      prevent_construction(event,error_collision)
+    local up_belt_name = up_belt_name_from_down(entity.name)
+    if not surface.can_place_entity { name = up_belt_name, position = entity.position } then
+      prevent_construction(event, error_collision)
       return
     end
 
-    local other_belt = surface.create_entity{
-      name="up-belt",
-      position=entity.position,
-      direction=flip_direction(entity.direction),
-      force=entity.force,
+    local other_belt = surface.create_entity {
+      name = up_belt_name,
+      position = entity.position,
+      direction = flip_direction(entity.direction),
+      force = entity.force,
     }
     if other_belt then
       other_belt.linked_belt_type = "output"
       entity.connect_linked_belts(other_belt)
     end
-  elseif entity.name == "up-belt" then
+  elseif entity.name:match('%w*up%-belt$') then
     entity.linked_belt_type = "input"
     local current_level = level_from_surface(entity.surface_index)
-   
+
     if not current_level then
       prevent_construction(event, error_no_z_level)
       return
@@ -154,16 +165,17 @@ script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_e
       global.surfaces[current_level + 1] = surface.index
     end
 
-    if not surface.can_place_entity{name="down-belt", position=entity.position} then
-      prevent_construction(event,error_collision)
+    local down_belt_name = down_belt_name_from_up(entity.name)
+    if not surface.can_place_entity { name = down_belt_name, position = entity.position } then
+      prevent_construction(event, error_collision)
       return
     end
 
-    local other_belt = surface.create_entity{
-      name="down-belt",
-      position=entity.position,
-      direction=flip_direction(entity.direction),
-      force=entity.force,
+    local other_belt = surface.create_entity {
+      name = down_belt_name,
+      position = entity.position,
+      direction = flip_direction(entity.direction),
+      force = entity.force,
     }
     if other_belt then
       other_belt.linked_belt_type = "output"
@@ -171,9 +183,9 @@ script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_e
     end
   elseif entity.name == "up-pole" then
     local current_level = level_from_surface(entity.surface_index)
-    
+
     if not current_level then
-      prevent_construction(event,error_no_z_level)
+      prevent_construction(event, error_no_z_level)
       return
     end
 
@@ -183,15 +195,15 @@ script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_e
       global.surfaces[current_level + 1] = surface.index
     end
 
-    if not surface.can_place_entity{name="down-pole", position=entity.position} then
-      prevent_construction(event,error_collision)
+    if not surface.can_place_entity { name = "down-pole", position = entity.position } then
+      prevent_construction(event, error_collision)
       return
     end
 
-    local other_pole = surface.create_entity{
-      name="down-pole",
-      position=entity.position,
-      force=entity.force,
+    local other_pole = surface.create_entity {
+      name = "down-pole",
+      position = entity.position,
+      force = entity.force,
     }
     if other_pole then
       other_pole.connect_neighbour(entity)
@@ -200,7 +212,7 @@ script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_e
     local current_level = level_from_surface(entity.surface_index)
 
     if not current_level then
-      prevent_construction(event,error_no_z_level)
+      prevent_construction(event, error_no_z_level)
       return
     end
 
@@ -210,23 +222,25 @@ script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_e
       global.surfaces[current_level - 1] = surface.index
     end
 
-    if not surface.can_place_entity{name="up-pole", position=entity.position} then
-      prevent_construction(event,error_collision)
+    if not surface.can_place_entity { name = "up-pole", position = entity.position } then
+      prevent_construction(event, error_collision)
       return
     end
 
-    local other_pole = surface.create_entity{
-      name="up-pole",
-      position=entity.position,
-      force=entity.force,
+    local other_pole = surface.create_entity {
+      name = "up-pole",
+      position = entity.position,
+      force = entity.force,
     }
-    other_pole.connect_neighbour(entity)
+    if other_pole then
+      other_pole.connect_neighbour(entity)
+    end
   end
 end)
 
-script.on_event({defines.events.on_player_mined_entity, defines.events.on_robot_mined_entity}, function(event)
+script.on_event({ defines.events.on_player_mined_entity, defines.events.on_robot_mined_entity }, function(event)
   local entity = event.entity
-  if entity.name == "up-belt" or entity.name == "down-belt" then
+  if entity.name:match('%w*down%-belt$') or entity.name:match('%w*up%-belt$') then
     local other_belt = entity.linked_belt_neighbour
     if other_belt then
       other_belt.destroy()
@@ -248,9 +262,9 @@ script.on_event({defines.events.on_player_mined_entity, defines.events.on_robot_
     end
 
     local target_entity
-    if entity.name == "up-pole" then 
+    if entity.name == "up-pole" then
       target_entity = "down-pole"
-    else 
+    else
       target_entity = "up-pole"
     end
     local other_pole = surface.find_entity(target_entity, entity.position)
@@ -265,7 +279,7 @@ script.on_event("3dbelt-down", function(event)
   local current_level = level_from_surface(player.surface_index)
 
   if not current_level then
-    player.create_local_flying_text{text="Unable to teleport from here",create_at_cursor=true}
+    player.create_local_flying_text { text = "Unable to teleport from here", create_at_cursor = true }
     return
   end
 
@@ -279,9 +293,9 @@ end)
 script.on_event("3dbelt-up", function(event)
   local player = game.players[event.player_index]
   local current_level = level_from_surface(player.surface_index)
-  
+
   if not current_level then
-    player.create_local_flying_text{text="Unable to teleport from here",create_at_cursor=true}
+    player.create_local_flying_text { text = "Unable to teleport from here", create_at_cursor = true }
     return
   end
 
@@ -289,5 +303,20 @@ script.on_event("3dbelt-up", function(event)
   if surface then
     player.teleport(player.position, surface)
     update_gui(player, current_level + 1)
+  end
+end)
+
+script.on_event("3dbelt-flip", function(event)
+  local player = game.players[event.player_index]
+  local entity = player.selected
+  if not entity then return end
+  if not (entity.name:match("%w*%-?down%-belt$") or entity.name:match("%w*%-?up%-belt$")) then return end
+  local other =entity.linked_belt_neighbour
+  if other then
+    entity.disconnect_linked_belts()
+    local t = entity.linked_belt_type
+    entity.linked_belt_type = other.linked_belt_type
+    other.linked_belt_type = t 
+    entity.connect_linked_belts(other)
   end
 end)
--- data-updates.lua
+++ data-updates.lua
@@ -1,135 +1,157 @@
-local downBeltItem = table.deepcopy(data.raw.item["underground-belt"])
+local function make_belt(source, name, color, order)
+    local beltItem = table.deepcopy(data.raw.item[source])
 
-downBeltItem.name = "down-belt"
-downBeltItem.place_result = "down-belt"
-downBeltItem.icon = "__3d-belts__/graphics/icons/down-belt.png"
+    beltItem.name = name
+    beltItem.place_result = name
+    beltItem.order = order
 
-local downBeltRecipe = table.deepcopy(data.raw["recipe"]["underground-belt"])
-downBeltRecipe.enabled = true
-downBeltRecipe.name = "down-belt"
-downBeltRecipe.result = "down-belt"
-data:extend{downBeltItem, downBeltRecipe}
+    beltItem.icons = {
+        {
+            icon = data.raw.item[source].icon,
+            icon_size = data.raw.item[source].icon_size,
+            icon_mipmaps = data.raw.item[source].icon_mipmaps,
+        },
+        {
+            icon = "__3d-belts__/graphics/icons/underground-belt-mask.png",
+            icon_size = 64,
+            tint = color,
+        }
+    }
 
-
-local downBeltEntity = table.deepcopy(data.raw["underground-belt"]["underground-belt"])
-downBeltEntity.name = "down-belt"
-downBeltEntity.type = "linked-belt"
-
-downBeltEntity.minable.result = "down-belt"
-downBeltEntity.max_distance = nil
-downBeltEntity.underground_sprite = nil
-downBeltEntity.underground_remove_belts_sprite = nil
-downBeltEntity.next_upgrade = nil
--- hsv 0.003, .57, -.1
-local downBeltSheet = "__3d-belts__/graphics/entity/down-belt/down-belt-structure.png"
-local downBeltSheetHr = "__3d-belts__/graphics/entity/down-belt/hr-down-belt-structure.png"
-downBeltEntity.structure.direction_in.sheet.filename = downBeltSheet
-downBeltEntity.structure.direction_in.sheet.hr_version.filename = downBeltSheetHr
-downBeltEntity.structure.direction_in_side_loading.sheet.filename = downBeltSheet
-downBeltEntity.structure.direction_in_side_loading.sheet.hr_version.filename = downBeltSheetHr
-downBeltEntity.structure.direction_out.sheet.filename = downBeltSheet
-downBeltEntity.structure.direction_out.sheet.hr_version.filename = downBeltSheetHr
-downBeltEntity.structure.direction_out_side_loading.sheet.filename = downBeltSheet
-downBeltEntity.structure.direction_out_side_loading.sheet.hr_version.filename = downBeltSheetHr
-
-data:extend{downBeltEntity}
+    local beltRecipe = table.deepcopy(data.raw["recipe"][source])
+    beltRecipe.enabled = true
+    beltRecipe.name = name
+    beltRecipe.result = name
 
 
+    local beltEntity = table.deepcopy(data.raw["underground-belt"][source])
+    beltEntity.name = name
+    beltEntity.type = "linked-belt"
+
+    beltEntity.allow_side_loading = true
+
+    beltEntity.minable.result = name
+    beltEntity.max_distance = nil
+    beltEntity.underground_sprite = nil
+    beltEntity.underground_remove_belts_sprite = nil
+    beltEntity.next_upgrade = nil
+
+    local colorMaskSprite = {
+        filename = "__3d-belts__/graphics/entity/hr-underground-belt-structure-mask.png",
+        tint = color,
+        width = 192,
+        height = 192,
+        scale = 0.5,
+        priority = "extra-high",
+        y = 192,
+    }
+    colorMaskSprite.hr_version = table.deepcopy(colorMaskSprite)
+    local colorMaskSpriteSide = {
+        filename = "__3d-belts__/graphics/entity/hr-underground-belt-structure-mask.png",
+        tint = color,
+        width = 192,
+        height = 192,
+        scale = 0.5,
+        priority = "extra-high",
+        y = 384,
+    }
+    colorMaskSpriteSide.hr_version = table.deepcopy(colorMaskSpriteSide)
+
+    local directions = {
+        {
+            name = "direction_in",
+            sprite = colorMaskSprite,
+        },
+        {
+            name = "direction_out",
+            sprite = colorMaskSprite,
+        },
+        {
+            name = "direction_in_side_loading",
+            sprite = colorMaskSpriteSide,
+        },
+        {
+            name = "direction_out_side_loading",
+            sprite = colorMaskSpriteSide,
+        }
+    }
+    for _, direction in pairs(directions) do
+        beltEntity.structure[direction.name].sheets = {
+            data.raw['underground-belt'][source].structure[direction.name].sheet,
+            direction.sprite,
+        }
+    end
+
+    return {
+        item = beltItem,
+        recipe = beltRecipe,
+        entity = beltEntity,
+    }
+end
 
 
-local upBeltItem = table.deepcopy(data.raw.item["underground-belt"])
+for _, variant in pairs({ "", "fast-", "express-" }) do
+    local source = variant .. "underground-belt"
+    local belt = make_belt(source, variant .. "down-belt", { r = 1, g = 0, b = 0, a = .8 },
+        "za-" .. data.raw.item[source].order .. "-z")
 
-upBeltItem.name = "up-belt"
-upBeltItem.place_result = "up-belt"
-upBeltItem.icon = "__3d-belts__/graphics/icons/up-belt.png"
+    data:extend { belt.item, belt.recipe }
+    data:extend { belt.entity }
+end
 
-local recipeUp = table.deepcopy(data.raw["recipe"]["underground-belt"])
-recipeUp.enabled = true
-recipeUp.name = "up-belt"
-recipeUp.result = "up-belt"
-data:extend{upBeltItem, recipeUp}
+for _, variant in pairs({ "", "fast-", "express-" }) do
+    local source = variant .. "underground-belt"
+    local belt = make_belt(source, variant .. "up-belt", { r = 0, g = 1, b = 0, a = .8 },
+        "zb-" .. data.raw.item[source].order .. "-z")
 
+    data:extend { belt.item, belt.recipe }
+    data:extend { belt.entity }
+end
 
-local upBeltEntity = table.deepcopy(data.raw["underground-belt"]["underground-belt"])
-upBeltEntity.name = "up-belt"
-upBeltEntity.type = "linked-belt"
+local function make_pole(source, name)
+    local pole = table.deepcopy(data.raw.item[source])
 
-upBeltEntity.minable.result = "up-belt"
-upBeltEntity.max_distance = nil
-upBeltEntity.underground_sprite = nil
-upBeltEntity.underground_remove_belts_sprite = nil
-upBeltEntity.next_upgrade = nil
--- hsv 0.32, 1, -.471
-local upBeltSheet = "__3d-belts__/graphics/entity/up-belt/up-belt-structure.png"
-local upBeltSheetHr = "__3d-belts__/graphics/entity/up-belt/hr-up-belt-structure.png"
-upBeltEntity.structure.direction_in.sheet.filename = upBeltSheet
-upBeltEntity.structure.direction_in.sheet.hr_version.filename = upBeltSheetHr
-upBeltEntity.structure.direction_in_side_loading.sheet.filename = upBeltSheet
-upBeltEntity.structure.direction_in_side_loading.sheet.hr_version.filename = upBeltSheetHr
-upBeltEntity.structure.direction_out.sheet.filename = upBeltSheet
-upBeltEntity.structure.direction_out.sheet.hr_version.filename = upBeltSheetHr
-upBeltEntity.structure.direction_out_side_loading.sheet.filename = upBeltSheet
-upBeltEntity.structure.direction_out_side_loading.sheet.hr_version.filename = upBeltSheetHr
+    pole.name = name
+    pole.place_result = name
+    pole.icon = "__3d-belts__/graphics/icons/" .. name .. ".png"
+    pole.order = "a[energy]-e[" .. name .. "]"
 
-data:extend{upBeltEntity}
+    local poleRecipe = table.deepcopy(data.raw["recipe"][source])
+    poleRecipe.enabled = true
+    poleRecipe.name = name
+    poleRecipe.result = name
+    data:extend { pole, poleRecipe }
 
 
-local downPoleItem = table.deepcopy(data.raw.item["medium-electric-pole"])
-
-downPoleItem.name = "down-pole"
-downPoleItem.place_result = "down-pole"
-downPoleItem.icon = "__3d-belts__/graphics/icons/down-pole.png"
-
-
-local downPoleRecipe = table.deepcopy(data.raw["recipe"]["medium-electric-pole"])
-downPoleRecipe.enabled = true
-downPoleRecipe.name = "down-pole"
-downPoleRecipe.result = "down-pole"
-data:extend{downPoleItem, downPoleRecipe}
+    local poleEntity = table.deepcopy(data.raw["electric-pole"][source])
+    poleEntity.name = name
+    poleEntity.minable.result = name
+    poleEntity.next_upgrade = nil
+    local poleSheet = "__3d-belts__/graphics/entity/" .. name .. "/" .. name .. ".png"
+    local poleSheetHr = "__3d-belts__/graphics/entity/" .. name .. "/hr-" .. name .. ".png"
+    poleEntity.pictures.layers[1].filename = poleSheet
+    poleEntity.pictures.layers[1].hr_version.filename = poleSheetHr
+    data:extend { poleEntity }
+end
 
 
-local downPoleEntity = table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
-downPoleEntity.name = "down-pole"
-downPoleEntity.minable.result = "down-pole"
-downPoleEntity.next_upgrade = nil
-local downPoleSheet = "__3d-belts__/graphics/entity/down-pole/down-pole.png"
-local downPoleSheetHr = "__3d-belts__/graphics/entity/down-pole/hr-down-pole.png"
-downPoleEntity.pictures.layers[1].filename = downPoleSheet
-downPoleEntity.pictures.layers[1].hr_version.filename = downPoleSheetHr
-data:extend{downPoleEntity}
-
-
-local upPole = table.deepcopy(data.raw.item["medium-electric-pole"])
-
-upPole.name = "up-pole"
-upPole.place_result = "up-pole"
-upPole.icon = "__3d-belts__/graphics/icons/up-pole.png"
-
-local upPoleRecipe = table.deepcopy(data.raw["recipe"]["medium-electric-pole"])
-upPoleRecipe.enabled = true
-upPoleRecipe.name = "up-pole"
-upPoleRecipe.result = "up-pole"
-data:extend{upPole, upPoleRecipe}
-
-
-local upPoleEntity = table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
-upPoleEntity.name = "up-pole"
-upPoleEntity.minable.result = "up-pole"
-upPoleEntity.next_upgrade = nil
-local upPoleSheet = "__3d-belts__/graphics/entity/up-pole/up-pole.png"
-local upPoleSheetHr = "__3d-belts__/graphics/entity/up-pole/hr-up-pole.png"
-upPoleEntity.pictures.layers[1].filename = upPoleSheet
-upPoleEntity.pictures.layers[1].hr_version.filename = upPoleSheetHr
-data:extend{upPoleEntity}
+make_pole("medium-electric-pole", "up-pole")
+make_pole("medium-electric-pole", "down-pole")
 
 data:extend({
-  {
-    type = "custom-input",
-    name = "3dbelt-up",
-    key_sequence = "CONTROL + UP",
-  },{
-    type = "custom-input",
-    name = "3dbelt-down",
-    key_sequence = "CONTROL + DOWN",
-  }
+    {
+        type = "custom-input",
+        name = "3dbelt-up",
+        key_sequence = "CONTROL + SHIFT + mouse-wheel-up",
+    },
+    {
+        type = "custom-input",
+        name = "3dbelt-down",
+        key_sequence = "CONTROL + SHIFT + mouse-wheel-down",
+    },
+    {
+        type = "custom-input",
+        name = "3dbelt-flip",
+        key_sequence = "F",
+    }
 })
